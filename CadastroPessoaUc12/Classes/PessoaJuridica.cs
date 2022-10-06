492df24e9bcf361b1398c0776cc9668478f2c6ad
AFRANIO DIMAS DA SILVA <86030954+AfranioD@users.noreply.github.com>
2022-10-06 00:44:50 -0300

    Add files via upload
    Adicionado arquivos criados durante o Encontro Remoto 5 SA2 da Uc-12.

---
+++ CadastroPessoaUc12/Classes/PessoaJuridica.cs
@@ -0,0 +1,56 @@
+using System.Text.RegularExpressions;
+using CadastroPessoaUc12.Interfaces;
+
+namespace CadastroPessoaUc12.Classes
+{
+    public class PessoaJuridica : Pessoa, IPessoaJuridica
+    {
+
+        public string? Cnpj { get; set; }
+
+        public string? RazaoSocial { get; set; }
+
+
+
+        public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        //xx.xxx.xxx/0001-xx
+
+        public bool ValidarCnpj(string cnpj)
+        {
+
+            //forma abaixo esta simplificada na condição logo abaixo.
+
+            // bool returnoCnpjValido = Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)");
+
+            // if (returnoCnpjValido == true)
+           
+            if (Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+            {
+                if (cnpj.Length == 18)
+                 {
+                    string subStringCnpj = cnpj.Substring(11, 4);
+
+                    if (subStringCnpj == "0001") 
+                    {
+                        return true;
+                    }
+
+                 }else if (cnpj.Length == 14) 
+                 {
+                    string subStringCnpj = cnpj.Substring(8, 4);
+
+                    if (subStringCnpj == "0001")
+                    {
+                        return true;
+                    }
+                 }
+            }
+            return false;
+
+        }
+    }
+}
